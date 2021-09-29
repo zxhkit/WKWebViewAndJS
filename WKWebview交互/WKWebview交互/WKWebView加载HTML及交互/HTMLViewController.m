62bd196c8c96c9da21e374b4f9fe8b6deac05c1c
周烜合 <zhouxh@icbobei.cn>
2021-09-29 18:41:27 +0800

    优化

--- WKWebview交互/WKWebview交互/WKWebView加载HTML及交互/HTMLViewController.m
+++ WKWebview交互/WKWebview交互/WKWebView加载HTML及交互/HTMLViewController.m
@@ -28,11 +28,8 @@
     
 }
 
-
-
-#pragma mark -
 #pragma mark - 初始化界面
--(void)setupUI {
+- (void)setupUI {
     
     UIView *navView = [[UIView alloc] initWithFrame:CGRectMake(0, kNavBarH, kScreenW, 45)];
     [self.view addSubview:navView];
@@ -50,8 +47,11 @@
         
     
 }
--(void)createWebView{
+- (void)createWebView{
     WKWebViewConfiguration *config = [[WKWebViewConfiguration alloc] init];
+    WKUserContentController *userController = [[WKUserContentController alloc] init];
+    config.userContentController = userController;
+    
     WKWebView *webView = [[WKWebView alloc] initWithFrame:CGRectMake(0, kNavBarH+45, kScreenW, kScreenH-kNavBarH-45) configuration:config];
     webView.navigationDelegate = self;
     webView.UIDelegate = self;
@@ -60,7 +60,11 @@
     WKUserScript*script = [[WKUserScript alloc] initWithSource:js injectionTime:WKUserScriptInjectionTimeAtDocumentEnd forMainFrameOnly:YES];
     [config.userContentController addUserScript:script];
     
-    [[webView configuration].userContentController addScriptMessageHandler:self name:@"show"];
+    // [[webView configuration].userContentController addScriptMessageHandler:self name:@"show"];
+    //或者
+    [userController addScriptMessageHandler:self name:@"show"];
+    [userController addScriptMessageHandler:self name:@"sayHello"];
+
     [self.view addSubview:webView];
     self.webView = webView;
     
@@ -76,7 +80,12 @@
     //    [_webView loadRequest:request];
 }
 
--(void)buttonClick{
+- (void)addScriptMessageHandler{
+    
+    
+}
+
+- (void)buttonClick{
     // iOS调用js里的navButtonAction方法并传入两个参数
     [self.webView evaluateJavaScript:@"navButtonAction('Xuanhe',25)" completionHandler:^(id _Nullable response, NSError * _Nullable error) {
         
@@ -89,19 +98,19 @@
 #pragma mark --------------------------------------------------
 #pragma mark - WWKNavigationDelegate
 //开始加载
--(void)webView:(WKWebView *)webView didStartProvisionalNavigation:(WKNavigation *)navigation{
+- (void)webView:(WKWebView *)webView didStartProvisionalNavigation:(WKNavigation *)navigation{
     //页面开始加载.
     
     //可以在这里做加载动画,然后在加载完成代理里面移除动画即可!
 }
 
 //网络错误
--(void)webView:(WKWebView *)webView didFailNavigation:(WKNavigation *)navigation withError:(NSError *)error{
+- (void)webView:(WKWebView *)webView didFailNavigation:(WKNavigation *)navigation withError:(NSError *)error{
     //网络报错
 }
 
 //网页加载完成
--(void)webView:(WKWebView *)webView didFinishNavigation:(WKNavigation *)navigation{
+- (void)webView:(WKWebView *)webView didFinishNavigation:(WKNavigation *)navigation{
     //设置JS
     NSString *js = @"document.getElementsByTagName('h1')[0].innerText";
     //执行JS
@@ -113,11 +122,11 @@
 
 #pragma mark --------------------------------------------------
 #pragma mark - WKUIDelegate
-// alert
+// alert 接收到警告面板
 //此方法作为js的alert方法接口的实现，默认弹出窗口应该只有提示信息及一个确认按钮，当然可以添加更多按钮以及其他内容，但是并不会起到什么作用
 //点击确认按钮的相应事件需要执行completionHandler，这样js才能继续执行
 ////参数 message为  js 方法 alert(<message>) 中的<message>
--(void)webView:(WKWebView *)webView runJavaScriptAlertPanelWithMessage:(NSString *)message initiatedByFrame:(WKFrameInfo *)frame completionHandler:(void (^)(void))completionHandler{
+- (void)webView:(WKWebView *)webView runJavaScriptAlertPanelWithMessage:(NSString *)message initiatedByFrame:(WKFrameInfo *)frame completionHandler:(void (^)(void))completionHandler{
     UIAlertController *alertController = [UIAlertController alertControllerWithTitle:@"提示" message:message?:@"" preferredStyle:UIAlertControllerStyleAlert];
     [alertController addAction:([UIAlertAction actionWithTitle:@"确认" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
         completionHandler();
@@ -127,7 +136,7 @@
 // confirm
 //作为js中confirm接口的实现，需要有提示信息以及两个相应事件， 确认及取消，并且在completionHandler中回传相应结果，确认返回YES， 取消返回NO
 //参数 message为  js 方法 confirm(<message>) 中的<message>
--(void)webView:(WKWebView *)webView runJavaScriptConfirmPanelWithMessage:(NSString *)message initiatedByFrame:(WKFrameInfo *)frame completionHandler:(void (^)(BOOL))completionHandler{
+- (void)webView:(WKWebView *)webView runJavaScriptConfirmPanelWithMessage:(NSString *)message initiatedByFrame:(WKFrameInfo *)frame completionHandler:(void (^)(BOOL))completionHandler{
     
     UIAlertController *alertController = [UIAlertController alertControllerWithTitle:@"提示" message:message?:@"" preferredStyle:UIAlertControllerStyleAlert];
     [alertController addAction:([UIAlertAction actionWithTitle:@"取消" style:UIAlertActionStyleCancel handler:^(UIAlertAction * _Nonnull action) {
@@ -163,12 +172,20 @@
 
 #pragma mark --------------------------------------------------
 #pragma mark - WLScriptMessageHandler
-//js传递过来的数据
--(void)userContentController:(WKUserContentController *)userContentController didReceiveScriptMessage:(WKScriptMessage *)message
-{
+//JS调用的OC回调方法.js传递过来的数据
+- (void)userContentController:(WKUserContentController *)userContentController didReceiveScriptMessage:(WKScriptMessage *)message{
+    
     NSLog(@"%@",message.name);//方法名
     NSLog(@"%@",message.body);//传递的数据
+    
+    if ([message.name isEqualToString:@"show"]) {
+        //JS调用的OC回调方法
+    }else if ([message.name isEqualToString:@"sayHello"]) {
+        NSLog(@"sayHello-你好");
+    }
+    
 }
+
 
 
 @end
